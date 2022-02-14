723f76cd41778f2ab13b21b0b7e6b748e06b427a
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2022-02-13 21:17:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 #Intiliaze a global value that will fetch the text file and store it in an array
-nickNames = list(open('nickname.txt', 'r')) 
+nickNames = list(open('nickname.txt', 'r'))
 
 
 #intitialize a function that will display the menu
-def menu():
-    print("hiiiii")
+def menu(firstName, lastName):
+  #print the the title of the menu
+  print("Main Menu (" + firstName + " " + lastName + ")")
 
-#initialize a function that will fetch the text data and store it into an array
-def dataProcessing(): 
-    nickNames.insert(1,"bye")
-    print(nickNames[1:3])
+  #initialize variable
+  loop = True 
+
+  #create a while loop that will loop through the menu
+  while loop:
+    userInp = int(input("Enter the number \n 1. Change Name \n 2. Display a Random Nickname \n 3. Display All Nicknames \n 4. Add a Nickname \n 5. Remove a Nickname \n 6. Exit  \n"))
+
+    if userInp == 1:
+      askName()
+
+    elif userInp == 2:
+      randomName()
+    
+    elif userInp == 3:
+      allName()
+    
+    elif userInp == 4:
+      addName()
+    
+    elif userInp == 5:
+      removeName()
+    
+    elif userInp == 6:
+      print("program closed")
+      loop = False
+    
+    else: 
+      print("Invalid Response")
 
 
-#initiliaze a main function that will invoke the other functions 
-def main():
-    menu()
-    dataProcessing()
+def askName():
+  firstName = input("Enter your first name: ")
+  lastName = input("Enter your last name: ")
+  menu(firstName, lastName)
 
-main()
+askName()
