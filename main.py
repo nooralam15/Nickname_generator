fd9900ad1d71503e1861549dedd43a4982af9180
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2022-02-13 20:27:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Intiliaze a global value that will store the nickname array
-global nickNames 
+#Intiliaze a global value that will fetch the text file and store it in an array
+nickNames = list(open('nickname.txt', 'r')) 
 
 
 #intitialize a function that will display the menu
@@ -8,9 +8,8 @@ def menu():
 
 #initialize a function that will fetch the text data and store it into an array
 def dataProcessing(): 
-    fetch = open('nickname.txt', 'r')
-    nickNames = list(fetch)
-    print(nickNames[1])
+    nickNames.insert(1,"bye")
+    print(nickNames[1:3])
 
 
 #initiliaze a main function that will invoke the other functions 
