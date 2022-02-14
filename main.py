083e526cd97a6db5e72dcb1eb3dcb21c6229b056
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2022-02-13 20:06:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#Intiliaze a global value that will store the nickname array
+global nickNames 
 
+
+#intitialize a function that will display the menu
+def menu():
+    print("hiiiii")
+
+#initialize a function that will fetch the text data and store it into an array
+def dataProcessing(): 
+    fetch = open('nickname.txt', 'r')
+    nickNames = list(fetch)
+    print(nickNames[1])
+
+
+#initiliaze a main function that will invoke the other functions 
+def main():
+    menu()
+    dataProcessing()
+
+main()
